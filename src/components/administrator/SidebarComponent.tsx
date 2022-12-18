37fb7665a7ed3de02d0d6504d489528ed6827c0b
sumania95 <ayansumania95@gmail.com>
2022-12-18 11:41:18 +0800

    fixed h-screen minus header

--- src/components/administrator/SidebarComponent.tsx
+++ src/components/administrator/SidebarComponent.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function SidebarComponent() {
   return (
     <div>
-        <div className='w-56 flex flex-col border-r h-screen p-2 space-y-1'>
+        <div className='w-56 flex flex-col border-r h-[calc(100vh-h-16)] p-2 space-y-1'>
           <Link href={'/user/administrator/'} className='w-full hover:bg-gray-200 p-2 hover:cursor-pointer'>DASHBOARD</Link>
           <Link href={'/user/administrator/approved'} className='w-full hover:bg-gray-200 p-2 hover:cursor-pointer'>APPROVED</Link>
           <Link href={'/user/administrator/genre'} className='w-full hover:bg-gray-200 p-2 hover:cursor-pointer'>GENRE</Link>

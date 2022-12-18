94b26bf53e86e1587f02c8b01893fa740da8cd8e
sumania95 <ayansumania95@gmail.com>
2022-12-18 23:40:28 +0800

    fixing bug build error

--- src/pages/administrator/sub-genre/index.tsx
+++ src/pages/administrator/sub-genre/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { requireAuth } from "../../../server/common/requireAuth";
 import Layout from '../../../components/administrator/Layout';
 import HeadCustom from '../../../components/HeadCustom';
-import SubGenreComponent from '../../../components/administrator/subgenre/SubGenreComponent';
+import SubGenreComponent from '../../../components/administrator/subgenre/SubGenreComponents';
 
 export const getServerSideProps = requireAuth(async (ctx) => {
     return { props: {} };
